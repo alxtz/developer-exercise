db698fd77258a25394534b51cfd65db9fd764ba2
Alex Tzeng <alxtz.tw@gmail.com>
2017-09-25 11:23:35 -0700

    Finished the marklar method, add develop log
    1. done by gsub and regex
    2. add developer log for how I wrote this
    
    TODO:
    1. finish the even_fibonacci method
    2. review the develop log

---
+++ DEVLOG.md
@@ -0,0 +1,13 @@
+# havn't developed in ruby before, but there should be couple of ways to do this
+
+1. keep the full sentence string and do something like the "replace()" method
+2. "split()" the str and put them back together
+
+The first method requires some regex, debugging both regex & ruby might be a pain for me(first time doing ruby), I'll go with the second method
+
+
+I think it would be something like spliting by spaces, but when marklarize the words, the none alphabet characters should be reserved
+
+But I found out the regex I tested with JavaScript is not even that hard, and with the secound method I still has to do "replace()" in something like "Trump(covfefe)"
+
+Some screenshots for trying out the working expression![](https://i.imgur.com/zetAuiK.png)
--- exercise.rb
+++ exercise.rb
@@ -1,10 +1,16 @@
 class Exercise
 
+
   # Assume that "str" is a sequence of words separated by spaces.
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
+
+
   def self.marklar(str)
     # TODO: Implement this method
+
+
+    return str.gsub(/\w{5,}/){ |input| /[[:upper:]]/.match(input[0]) ? 'Marklar' : 'marklar' }
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -16,3 +22,8 @@ class Exercise
   end
 
 end
+
+# Testing the result
+puts(Exercise.marklar('The quick brown fox'))
+puts(Exercise.marklar('Down goes Frazier'))
+puts(Exercise.marklar('How is the weather today? I have not been outside.'))
