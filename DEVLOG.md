02818c8fd454fb179ff5f61496835dbe3fb1784b
Alex Tzeng <alxtz.tw@gmail.com>
2017-09-25 12:12:50 -0700

    Finished the even_fibonacci method
    1. fill the even_fibonacci, add a method called create_fibonacci
    2. finished the devlog

--- DEVLOG.md
+++ DEVLOG.md
@@ -1,4 +1,6 @@
-# havn't developed in ruby before, but there should be couple of ways to do this
+# marklar
+
+havn't developed in ruby before, but there should be couple of ways to do this
 
 1. keep the full sentence string and do something like the "replace()" method
 2. "split()" the str and put them back together
@@ -11,3 +13,7 @@ I think it would be something like spliting by spaces, but when marklarize the w
 But I found out the regex I tested with JavaScript is not even that hard, and with the secound method I still has to do "replace()" in something like "Trump(covfefe)"
 
 Some screenshots for trying out the working expression![](https://i.imgur.com/zetAuiK.png)
+
+# even_fibonacci
+
+I tried to create an array of fibonacci sequence first, and just sum the even numbers together
--- exercise.rb
+++ exercise.rb
@@ -1,29 +1,47 @@
 class Exercise
 
 
-  # Assume that "str" is a sequence of words separated by spaces.
-  # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
-  # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
-
-
   def self.marklar(str)
-    # TODO: Implement this method
-
 
     return str.gsub(/\w{5,}/){ |input| /[[:upper:]]/.match(input[0]) ? 'Marklar' : 'marklar' }
   end
 
-  # Return the sum of all even numbers in the Fibonacci sequence, up to
-  # the "nth" term in the sequence
-  # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
-  # and the sum of its even numbers is (2 + 8) = 10
+
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
+
+    fib_array = create_fibonacci(nth)
+
+    sum = 0
+    fib_array.each { |x|
+      if (x%2==0)
+        sum += x
+      end
+    }
+
+    # puts('sum',sum)
+    return sum
+  end
+
+  def self.create_fibonacci(nth)
+    index = 0
+
+    ary = []
+    a, b = 0, 1
+    while index < nth
+      index = index + 1
+      ary.push(a)
+      a, b = b, a+b
+    end
+
+    return ary
   end
 
 end
 
 # Testing the result
-puts(Exercise.marklar('The quick brown fox'))
-puts(Exercise.marklar('Down goes Frazier'))
-puts(Exercise.marklar('How is the weather today? I have not been outside.'))
+
+# puts(Exercise.marklar('The quick brown fox'))
+# puts(Exercise.marklar('Down goes Frazier'))
+# puts(Exercise.marklar('How is the weather today? I have not been outside.'))
+
+# Exercise.even_fibonacci(35)
